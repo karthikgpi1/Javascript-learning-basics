c933d490d5dcc5fdc8a391745de1cd8358b20eb6
karthikgpi1 <karthikgopi024@gmail.com>
2022-06-24 21:38:38 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// function kar(x = 40, y = 10) {
+//     if (x > 30) {
+//        return x*5;
+//     }
+//     return y*5;
+// }
+
+// var x = kar(30);
+// console.log(x);
+
+
+
+
+// var kar = function (x = 12, y = 30) {
+//     console.log(x / y );
+// };
+
+// console.log(kar);
+// kar(90, 6);
+
+
+
+// iife
+
+// (function (value1, value2 = 5) {
+
+//     console.log(value1 > value2);
+    
+
+// })(60);
+
+
+
+
